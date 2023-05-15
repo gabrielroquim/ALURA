25a0a8e73c1eddcca80ad5ceba354316dc75f7f7
Gabriel Roquim <gabrielroquim@hotmail.com>
2023-05-15 20:59:14 -0300

    aula 9

--- Autenticacao_seguranca/testes_unitarios_integracao/carrinho/test/carrinho.test.js
+++ Autenticacao_seguranca/testes_unitarios_integracao/carrinho/test/carrinho.test.js
@@ -23,7 +23,39 @@ describe('Testes do Carrinho', () => {
 
     it('Deve ter a propriedade "total" na inicialização', () => {
         const carrinho = new Carrinho();
-        
+
         expect(carrinho).toHaveProperty('total');
+    });
+
+    it('Deve lançar erro ao finalizar compra com carrinho vazio', () => {
+
+        function englobaErroCarrinho() {
+            const carrinho = new Carrinho();
+            carrinho.finalizaCompra();
+        }
+        expect(englobaErroCarrinho).toThrowError('Carrinho de compras vazio');
+    });
+
+    it('Deve Adicionar o frete', () => {
+        const carrinho = new Carrinho();
+        carrinho.adicionaFrete(10);
+
+        expect(carrinho.frete).toBe(10);
+    });
+
+    it('Deve finalizar as compras', () => {
+        const item = new Item('Banana', 2, 5);
+        const item2 = new Item('Mel', 1, 5);
+
+        const carrinho = new Carrinho();
+        carrinho.adiciona(item);
+        carrinho.adiciona(item2);
+        carrinho.adicionaFrete(10);
+
+        expect(carrinho.finalizaCompra()).toStrictEqual({
+            subtotal:15,
+            frete:10,
+            total:25
+        });
     });
 });
